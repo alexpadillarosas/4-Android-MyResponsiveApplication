788d004d4ca2b820a2a22f9b501e43c2f725708b
Alex Padilla <alex@abc.com>
2019-11-27 11:26:22 +1100

    Initial commit

---
+++ app/src/main/java/com/example/android/myresponsiveapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.myresponsiveapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // to check about constraint layout
+    //    https://www.youtube.com/watch?v=P9Zstbk0lPw
+    //    https://www.youtube.com/watch?v=rzmB3UxxhaA
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.layout2);
+    }
+}
